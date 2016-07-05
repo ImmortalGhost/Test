c13895f917c11dbc1d23a65bdefafe9abf7af08a
ImmortalGhost <ImmortalGhost@yandex.ru>
2016-07-06 01:45:08 +0300

    Initial commit

---
+++ src/main/java/Test.java
@@ -0,0 +1,10 @@
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * Created by Владос on 06.06.2016.
+ */
+public class Test {
+    public static void main (String[] args) {
+
+    }
+}
